5b7e3a7686d9a13ee6594506ae9c4d34c7798e8c
Maria Luisa Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2022-05-15 14:44:57 -0300

    Update aula02-extra01.js

--- aula02-extra01.js
+++ aula02-extra01.js
@@ -1,6 +1,6 @@
 //EXERCÍCIO EXTRA 1
 
-let chosenString = 'metal';
+let chosenString = "metal";
 let size = chosenString.length;
 let result = "";
 let vowelNum = 0;
@@ -10,25 +10,27 @@ function makeReport() {
     let letters = chosenString.charAt(index);
     result = result + letters;
   }
-    for (index = 0; index <= size; index++) {
-  switch (chosenString[index]) {
-      case "a" :
-      case "e" :
-      case "i" :
-      case "o" :
-      case "u" :
-      case "A" :
-      case "E" :
-      case "I" :
-      case "O" :
-      case "U" :
-          vowelNum++;
-  break;
-  }}
+  for (index = 0; index <= size; index++) {
+    switch (chosenString[index]) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+      case "A":
+      case "E":
+      case "I":
+      case "O":
+      case "U":
+        vowelNum++;
+        break;
+    }
+  }
   return {
-   result: `Inverso da string é ${result}` ,
-   num : `O número de vogais é ${vowelNum}`
-  }}
-  
+    result: `Inverso da string é ${result}`,
+    num: `O número de vogais é ${vowelNum}`,
+  };
+}
+
 console.log(`A string escolhida é ${chosenString}`);
 console.log(makeReport());
