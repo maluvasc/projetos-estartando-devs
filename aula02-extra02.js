9e3932a0516cb93ab2970a92fa62c2caebda7306
Maria Luisa Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2022-05-15 14:46:07 -0300

    Update aula02-extra02.js

--- aula02-extra02.js
+++ aula02-extra02.js
@@ -10,30 +10,31 @@ function makeReport() {
     let letters = chosenString.charAt(index);
     result = result + letters;
   }
-    for (index = 0; index <= size; index++) {
-  switch (chosenString[index]) {
-      case "a" :
-      case "e" :
-      case "i" :
-      case "o" :
-      case "u" :
-      case "A" :
-      case "E" :
-      case "I" :
-      case "O" :
-      case "U" :
-          vowelNum++;
-  break;
-  }}
-  return {
-   result: `Inverso da string é ${result}` ,
-   num : `O número de vogais é ${vowelNum}`
-  }}
-  
-if (typeof chosenString === 'string') {
-  console.log(`A string escolhida é ${chosenString}`)
-  console.log(makeReport());
-  } else {
-    console.log("oh carai");
+  for (index = 0; index <= size; index++) {
+    switch (chosenString[index]) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+      case "A":
+      case "E":
+      case "I":
+      case "O":
+      case "U":
+        vowelNum++;
+        break;
+    }
   }
-  
+  return {
+    result: `Inverso da string é ${result}`,
+    num: `O número de vogais é ${vowelNum}`,
+  };
+}
+
+if (typeof chosenString === "string") {
+  console.log(`A string escolhida é ${chosenString}`);
+  console.log(makeReport());
+} else {
+  console.log("oh carai");
+}
