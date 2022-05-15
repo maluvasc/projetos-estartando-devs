a4317f44973f8295f6e95e11630b152a3a596b9d
Maria Luisa Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2022-05-15 14:43:29 -0300

    Update aula02-ex02.js

--- aula02-ex02.js
+++ aula02-ex02.js
@@ -6,21 +6,23 @@ let size = chosenString.length;
 
 function countNumberOfVowels() {
   for (index = 0; index <= size; index++) {
-switch (chosenString[index]) {
-    case "a" :
-    case "e" :
-    case "i" :
-    case "o" :
-    case "u" :
-    case "A" :
-    case "E" :
-    case "I" :
-    case "O" :
-    case "U" :
+    switch (chosenString[index]) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+      case "A":
+      case "E":
+      case "I":
+      case "O":
+      case "U":
         vowelNum++;
-break;
-}}
-return {
- num : `O número de vogais é ${vowelNum}`
-}}
- console.log(countNumberOfVowels())
+        break;
+    }
+  }
+  return {
+    num: `O número de vogais é ${vowelNum}`,
+  };
+}
+console.log(countNumberOfVowels());
